b4ea355bd12a861c6ab1797c42b3c5bb14f90155
ntridip818-web <ntridip818@gmail.com>
2026-09-04 18:30:00 +0530

    Allow cleanup authentication header through CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     allow_origins=cors_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_headers=["Authorization", "Content-Type", "X-Cleanup-Token"],
 )
 
 
